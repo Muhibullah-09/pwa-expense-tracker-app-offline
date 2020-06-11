114e626eb4b4f9811fb5bf76e6be85cb92728b7b
Muhibullah-09 <mc2840696@gmail.com>
2020-06-11 18:49:27 +0500

    Update

---
+++ src/Head.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Head.css';
+
+function Name(){
+    reutrn (
+        <h2 className='pin'>A Expense Tracker App in PKR by Muhiullah Khan Kamali</h2>
+    );
+}
+export default Head;
--- src/context/Head.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function Head(){
-    return (
-        <h2>A Expense Tracker in PKR  By Muhibullah Khan Kamali</h2>
-    );
-}export default Head;
-
