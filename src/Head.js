5eb53c75d9afd89bfa6eda2d1893412a7b00abbd
Muhibullah-09 <mc2840696@gmail.com>
2020-06-11 18:58:30 +0500

    Add Css

--- src/Head.js
+++ src/Head.js
@@ -3,7 +3,7 @@ import './Head.css';
 
 function Head(){
     return (
-        <h2 className='pin'>A Expense Tracker App in PKR by Muhiullah Khan Kamali</h2>
+        <h2 className='pin'>A Expense Tracker App in PKR by Muhibullah Khan Kamali</h2>
     );
 }
 export default Head;
