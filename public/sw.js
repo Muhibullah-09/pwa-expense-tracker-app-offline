2a5adf10fe5e0356ea1fd6c137498031414f2fa2
Muhibullah-09 <mc2840696@gmail.com>
2020-08-08 18:09:38 +0500

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -4,9 +4,7 @@
 const CACHE_NAME = "version-1";
 
 //here offline.html is used when we offline
-const urlsToCache = [
-    '/src'
-];
+const urlsToCache = ['index.html', '/src/index.js'];
 
 const self = this;
 
@@ -28,7 +26,7 @@ self.addEventListener('fetch', (event) => {
         caches.match(event.request)//here request means EX: you need to see a image you send request
             .then(() => {
                 return fetch(event.request)
-                    .catch(() => caches.match('/index.js'))
+                    .catch(() => caches.match('/src/index.js'))
             })
     )
 });
